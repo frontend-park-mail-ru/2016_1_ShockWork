29f85a50b4c96266553053848c322f8ab3578b0f
Vladislav <shramru@gmail.com>
2016-02-15 16:43:13 +0300

    homework0

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 var express = require('express'),
-    errorHandler = require('errorhandler'),
-    app = express();
+	errorHandler = require('errorhandler'),
+	app = express();
 
 var HOSTNAME = 'localhost',
-    PORT = 8080,
-    PUBLIC_DIR = __dirname + '/public_html';
+	PORT = 8080,
+	PUBLIC_DIR = __dirname + '/public_html';
 
-app.use(function (req) {
+var callback = function (req, res, done) {
 	// Здесь нужно написать журналирование в формате
 	// (журналирование - вывод в консоль)
 	// [время] [номер запроса по счету]
-});
+
+	console.log("[%s] [%s]", new Date(), ++callback.counter);
+	done();
+}
+
+callback.counter = 0;
+app.use(callback);
 
 app
 	.use('/', express.static(PUBLIC_DIR))
