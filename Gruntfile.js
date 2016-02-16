552884519e52f1ddeb7e19797b9ae7cb24dcee16
sp41mer <sp41mer@yahoo.com>
2016-02-16 10:59:36 +0300

    gruntfile like prepods want Нормальный грунтфайл, надеюсь тут можно на русском писать

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,30 @@
 module.exports = function (grunt) {
 
+
     grunt.initConfig({
 
 		shell: {
 			// запуск сервера через скрипт shell'a https://www.npmjs.com/package/grunt-shell
+            dev: {
+                command: 'node server.js'
+            }
 		},
 
 		watch: {
 			// запуск watcher'a, который следит за изенениями файлов  templates/*.xml
 			// и если они изменяются, то запускает таск сборки шаблонов (grunt fest)
+            files: ['templates/*.xml'],
+            tasks: ['fest']
 		},
 		
 		concurrent: {
 			// одновременный запуска shell'a и watcher'a https://www.npmjs.com/package/grunt-concurrent
+            target: {
+                tasks: ['shell:dev', 'watch'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            }
 		},
 
 		fest: {
@@ -43,5 +55,6 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-fest');
 
     // результат команды grunt
-    grunt.registerTask('default', ['shell', 'watch']);
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.registerTask('default', ['concurrent:target']);
 };
