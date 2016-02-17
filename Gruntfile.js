72a472ceade5dc377b880ae0e46ea2ea533afe51
Vladislav <shramru@gmail.com>
2016-02-17 15:02:47 +0300

    grunt update

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function (grunt) {
     grunt.initConfig({
 
 		shell: {
-            server: {
+            dev: {
                 command: 'node server.js'
             }
 			// запуск сервера через скрипт shell'a https://www.npmjs.com/package/grunt-shell
@@ -12,12 +12,20 @@ module.exports = function (grunt) {
 		watch: {
 			// запуск watcher'a, который следит за изенениями файлов  templates/*.xml
 			// и если они изменяются, то запускает таск сборки шаблонов (grunt fest)
+            files: ['templates/*.xml'],
+            tasks: ['fest']
 		},
 		
 		concurrent: {
 			// одновременный запуска shell'a и watcher'a https://www.npmjs.com/package/grunt-concurrent
-		},
-
+            target: {
+                tasks: ['shell:dev', 'watch'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            },
+        },
+        
 		fest: {
             templates: {
                 files: [{
@@ -46,5 +54,6 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-fest');
 
     // результат команды grunt
-    grunt.registerTask('default', ['shell', 'watch']);
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.registerTask('default', ['concurrent:target']);
 };
