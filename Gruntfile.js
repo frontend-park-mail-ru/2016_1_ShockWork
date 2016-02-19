27dd1a81ba6fcf04aac0e9e5a6956c022993c1f1
sp41mer <sp41mer@yahoo.com>
2016-02-19 19:31:01 +0300

    yep, now always's right and it looks like v2

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,28 @@
 module.exports = function (grunt) {
 
-
     grunt.initConfig({
 
-		shell: {
-			// запуск сервера через скрипт shell'a https://www.npmjs.com/package/grunt-shell
+        shell: {
             dev: {
                 command: 'node server.js'
             }
-		},
+        },
 
-		watch: {
-			// запуск watcher'a, который следит за изенениями файлов  templates/*.xml
-			// и если они изменяются, то запускает таск сборки шаблонов (grunt fest)
+        watch: {
             files: ['templates/*.xml'],
             tasks: ['fest']
-		},
-		
-		concurrent: {
-			// одновременный запуска shell'a и watcher'a https://www.npmjs.com/package/grunt-concurrent
+        },
+
+        concurrent: {
             target: {
                 tasks: ['shell:dev', 'watch'],
                 options: {
                     logConcurrentOutput: true
                 }
             }
-		},
+        },
 
-		fest: {
+        fest: {
             templates: {
                 files: [{
                     expand: true,
@@ -43,20 +38,22 @@ module.exports = function (grunt) {
                         );
                     }
                 }
+            },
+            qunit: {
+                all: ['./public_html/tests/index.html']
             }
         }
-
     });
 
-	// подключть все необходимые модули
+    // подключть все необходимые модули
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.loadNpmTasks('grunt-concurrent');
-	grunt.loadNpmTasks('grunt-shell');
-	grunt.loadNpmTasks('grunt-fest');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-fest');
 
     // результат команды grunt
-    grunt.loadNpmTasks('grunt-concurrent');
     grunt.registerTask('default', ['concurrent:target']);
+    grunt.registerTask('test', ['qunit:all']);
 
 };
